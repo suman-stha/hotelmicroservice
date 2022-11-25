64c415d3f177dfe2d0bd6aca6e15da7f72006759
suman-stha <shresthasuman919@gmail.com>
2022-11-24 20:39:48 -0500

    updated

--- rating-service/src/main/java/com/ratingservice/service/impl/RatingServiceImpl.java
+++ rating-service/src/main/java/com/ratingservice/service/impl/RatingServiceImpl.java
@@ -8,6 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.UUID;
 
 @Service
 public class RatingServiceImpl implements RatingService {
@@ -18,6 +19,8 @@ public class RatingServiceImpl implements RatingService {
 
     @Override
     public Rating create(Rating rating) {
+        String ratingId = UUID.randomUUID().toString();
+        rating.setRatingId(ratingId);
         return repository.save(rating);
     }
 
--- user-service/src/main/java/com/userservice/entities/User.java
+++ user-service/src/main/java/com/userservice/entities/User.java
@@ -19,7 +19,7 @@ import java.util.List;
 public class User {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+
     @Column(name = "userId")
     private String userId;
 
